bb2daea2a2221876f9c8bc2eb06c212eb55cb667
Alexei Kireev <durobotdev@gmail.com>
2024-10-09 12:37:19 +0300

    Modified build.zig to catch up with Zig build system development

--- build.zig
+++ build.zig
@@ -19,7 +19,7 @@ pub fn build(b: *std.Build) void
 //     const mod = b.addModule("zigStructPrint", // package_name
     _ = b.addModule("zigStructPrint", // package_name
     .{
-        .root_source_file = .{ .path = "src/zsp.zig" },
+        .root_source_file = b.path("src/zsp.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -29,7 +29,7 @@ pub fn build(b: *std.Build) void
         .name = "zigStructPrint",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/zsp.zig" },
+        .root_source_file = b.path("src/zsp.zig"),
         .target = target,
         .optimize = optimize,
     });
